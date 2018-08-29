10f92a8c9780ccedb0c8b5ccc725a999da35e176
Dan Hintz <dhintz@live.com>
2018-08-29 22:58:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.size == 0
     return nil
   else
-    key, value = hash.first
-    smallest_key = key
-    smallest_key_num = value
+    smallest_key, smallest_key_num = hash.first
     name_hash.each do |key, num|
       if num < smallest_key_num
         smallest_key_num = num
